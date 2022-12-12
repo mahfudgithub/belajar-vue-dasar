fc1c38a5e1b36f3234b78f007cc3aea8bd961dfa
mahfudin <mahfudrahman14@gmail.com>
2022-12-12 08:48:10 +0700

    change

--- src/App.vue
+++ src/App.vue
@@ -1,52 +1,20 @@
 <template>
-  <h2>Nama : {{ nama }}</h2>
-  <button @click="changeName">Ubah Nama</button>
-  <div class="row"></div>
-  <a v-bind:href="url">go to google</a>
-  <div class="row"></div>
-  <input type="text" v-model="name" />
-  <br />
-  {{ name }}
-  <div v-if="products.length">
-    <ul>
-      <li v-for="item in products" :key="item.id">{{ item.title }} - {{ item.price }}</li>
-    </ul>
-  </div>
-  <div v-else>
-    <p>Data tidak ditemukan</p>
-  </div>
+  <Header :text="text" />
 </template>
 
 <script>
+import Header from "@/components/Header.vue";
 export default {
   name: "App",
+  components: {
+    Header,
+  },
   data() {
     return {
-      nama: "Myria Camellia",
-      url: "https://google.com",
-      name: "Myria",
-      products: [
-        { id: 1, title: "Product 1", price: 2000 },
-        { id: 2, title: "Product 2", price: 3000 },
-        { id: 3, title: "Product 3", price: 1000 },
-      ],
+      text: "ini data text",
     };
-  },
-  methods: {
-    changeName() {
-      this.nama = "Mahfudin";
-    },
   },
 };
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
